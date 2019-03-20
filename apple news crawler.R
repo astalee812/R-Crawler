6e7932b49226a290f3b7b95f256a897163ea3911
Asta Lee <astalee12@gmail.com>
2019-03-20 22:45:28 +0800

    Update apple news crawler.R

--- apple news crawler.R
+++ apple news crawler.R
@@ -1,1 +1,25 @@
-setwd("C:/Users/ASUS/Desktop/apple news crawler")
+setwd("C:/Users/ASUS/Desktop/crawler")
+#主要做網路爬蟲的套件:rvest
+install.packages("rvest")
+library(rvest)
+install.packages("magrittr")
+library(magrittr)
+
+#從apple news的最新消息做練習
+appleweb<-"https://tw.appledaily.com/new/realtime/"
+applenews<-read_html(appleweb,encoding = "UTF-8")
+
+#抓取新聞的時間
+time<-applenews%>%html_nodes("time")%>%html_text()
+time
+
+#抓取新聞的title
+title<-applenews%>%html_nodes("h1")%>%html_text()
+title
+
+#抓取新聞的類別
+category<-applenews%>%html_nodes("h2")%>%html_text()
+category
+
+
+
