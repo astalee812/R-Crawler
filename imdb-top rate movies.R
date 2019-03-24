56056555aef083fb3619c116fa432c997b137d25
Asta Lee <astalee12@gmail.com>
2019-03-24 23:50:40 +0800

    Update imdb-top rate movies.R

--- imdb-top rate movies.R
+++ imdb-top rate movies.R
@@ -16,13 +16,28 @@ imdb_movie<-imdb_movie[imdb_movie!=" "]
 imdb_movie<-as.data.frame(imdb_movie)
 imdb_movie<-imdb_movie[c(2:251),]
 imdb_movie
-
+#抓上映年份
 imdb_year<-imdbweb%>%html_nodes(".secondaryInfo")%>%html_text()
 imdb_year
-
+#抓評價
 imdb_rate<-imdbweb%>%html_nodes("strong")%>%html_text()
 imdb_rate<-as.data.frame(imdb_rate)
 imdb_rate<-imdb_rate[5:254,]
 
+#把所有訊息做成一份表格
 imdb_list<-data.frame(name=imdb_movie,year=imdb_year,rate=imdb_rate)
 write.csv(imdb_list,file = "imdb_list.csv")
+
+#試看看抓取圖片
+imdb_pic<-imdbweb%>%html_nodes(".posterColumn")%>%html_nodes("img")%>%html_attr("src")
+head(imdb_pic,10)
+#先新增一個資料夾放要照片的
+dir.create("C:/Users/ASUS/Desktop/crawler/pic/")
+install.packages("downloader")
+library(downloader)
+#使用for迴圈把所有的照片下載下來
+for(i in 1:length(imdb_pic))
+{
+  download(imdb_pic[i],paste("C:/Users/ASUS/Desktop/crawler/pic/pic",i,".jpg",sep = ""),mode="wb")
+  }
+
